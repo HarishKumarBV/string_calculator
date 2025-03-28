40111f5d86247d12006a35f52c44ef81363d74da
Harish Kumar B V <harish.dgs@gmail.com>
2025-03-29 00:23:35 +0530

    feat: sum two comman-seperated numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,5 +2,7 @@ class StringCalculator
     def add(input)
         return 0 if input.empty?
         return input.to_i if input.match?(/^\d+$/)
+        numbers = input.split(',').map(&:to_i)
+        numbers.sum
     end
 end 
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,8 +6,13 @@ RSpec.describe StringCalculator do
         it 'returns 0 for an empty string' do
             expect(calculator.add('')).to eq(0)
         end
+        
         it 'returns the number for a single number string' do
             expect(calculator.add('5')).to eq(5)
+        end
+
+        it 'returns the sum of two numbers' do
+            expect(calculator.add('1,2')).to eq(3)
         end
     end
 end
