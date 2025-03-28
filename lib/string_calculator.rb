7614a15e2686eb502208041852105cc2ee13e80a
Harish Kumar B V <harish.dgs@gmail.com>
2025-03-29 00:43:53 +0530

    feat: support custom delimiters using //[delimiter]\n[numbers]

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,17 @@
 class StringCalculator
     def add(input)
         return 0 if input.empty?
-        # return input.to_i if input.match?(/^\d+$/)
-        numbers = input.split(/[\n,]/).map(&:to_i)
+        # return input.to_i if input.match?(/^\d+$/) # This line is not needed as we handle it in the map below
+        # numbers = input.split(/[\n,]/).map(&:to_i)
+
+        delimiter = /[\n,]/ # Default delimiters
+        if input.start_with?('//')
+            parts = input.split("\n", 2)
+            custom_delimiter = parts[0][2..-1] # Extract the custom delimiter
+            input = parts[1] # The rest is the numbers
+            delimiter = Regexp.new(Regexp.escape(custom_delimiter)) # Create a regex for the custom delimiter
+        end
+        numbers = input.split(/#{delimiter}/).map(&:to_i)
         print numbers.inspect
         numbers.sum
     end
